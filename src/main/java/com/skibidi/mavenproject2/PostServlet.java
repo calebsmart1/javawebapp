5c82cf177806a73d7fa799cad9bd86f02dc20247
= <caleb.osunagarcia1@gmail.com>
2024-11-27 09:53:51 -0500

    First files!

---
+++ src/main/java/com/skibidi/mavenproject2/PostServlet.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.skibidi.mavenproject2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author caris
+ */
+@WebServlet(name = "PostServlet", urlPatterns = {"/post"})
+public class PostServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>You typed: " + name + "</h1>");
+    }
+}
